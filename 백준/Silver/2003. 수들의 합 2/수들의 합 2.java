4dccfde7809b53055ff3f80f2a6c3ceccf189895
miso <88221290+kmsaidev@users.noreply.github.com>
2022-07-19 14:00:46 +0900

    [Silver IV] Title: 수들의 합 2, Time: 160 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,35 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[] A;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         int result = 0;
         for(int i = 0; i < N; i++)
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
 
         int low = 0;
-        int high = 1;
-        int sum;
+        int high = 0;
+        int sum = A[0];
 
-        while(high <= A.length){
-            sum = 0;
-            for(int i = low; i < high; i++)
-                sum += A[i];
-            if(sum < M)
-                high++;
+        while(true){
+            if(sum < M) {
+                sum += A[++high];
+            }
             else if(sum == M){
                 result++;
-                low++;
+                sum -= A[low++];
             }
-            else low++;
+            else sum -= A[low++];
+            if(high == N)
+                break;
         }
-
         System.out.println(result);
     }
 }
